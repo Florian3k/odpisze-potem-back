e000e1417a232126f722e9aff03f8b1db555091c
Florian3k <24961583+Florian3k@users.noreply.github.com>
2019-03-12 11:20:40 +0100

    update GET /

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get } from '@nestjs/common';
 @Controller()
 export class AppController {
   @Get()
-  getHello(): string {
-    return 'Welcome to odpisze-potem-back api';
+  getHello() {
+    return { info: 'Welcome to odpisze-potem-back api' };
   }
 }
