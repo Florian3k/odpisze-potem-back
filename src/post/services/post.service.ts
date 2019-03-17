e019fbfe60febeecc1173190b619a753d9710520
Florian3k <24961583+Florian3k@users.noreply.github.com>
2019-03-17 20:35:20 +0100

    add createdAt to post model

--- src/post/services/post.service.ts
+++ src/post/services/post.service.ts
@@ -8,11 +8,13 @@ export class PostService {
       id: 'a2bbd45b-f1e5-48ad-bcb7-b0a4e990196a',
       title: 'Hello World',
       content: 'Lorem ipsum dolor sit amet',
+      createdAt: new Date(),
     },
     {
       id: 'c605a343-1cbb-4250-8534-92d42eac6dd3',
       title: 'Yeah! It works!',
       content: 'n/a',
+      createdAt: new Date(),
     },
   ];
 
