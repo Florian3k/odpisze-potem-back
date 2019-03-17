ff675724fb935585ad867f8caaa244c0222ea147
Florian3k <24961583+Florian3k@users.noreply.github.com>
2019-03-17 19:44:35 +0100

    move typeorm config to dbmodule and .env

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -5,7 +5,19 @@ import { resolve } from 'path';
 @Global()
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      synchronize: true,
+      logging: false,
+      entities: ['src/**/*.entity.ts'],
+      migrations: ['src/migration/**/*.ts'],
+      subscribers: ['src/subscriber/**/*.ts'],
+    }),
   ],
 })
 export class DbModule {}
