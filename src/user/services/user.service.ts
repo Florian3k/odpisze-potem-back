ed0f7f51d8ad22d86577fbee3558010811806fb0
Florian3k <24961583+Florian3k@users.noreply.github.com>
2019-03-20 16:02:47 +0100

    create createUser method

--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { UserEntity } from '../entities';
+import { UserModel } from '../models';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  async createUser(
+    nickname: string,
+    plaintextPassword: string,
+    email: string,
+  ): Promise<UserModel> {
+    const collidingUsers = await this.userRepository.find({
+      where: [{ nickname }, { email }],
+    });
+    if (collidingUsers.length !== 0) {
+      throw new UnprocessableEntityException(
+        'User with given nickname/email exists',
+      );
+    }
+    const hashedPassword = await bcrypt.hash(plaintextPassword, 10);
+    const createdUser = this.userRepository.create({
+      nickname,
+      email,
+      password: hashedPassword,
+    });
+    const {
+      password,
+      registeredAt,
+      // tslint:disable-next-line: trailing-comma
+      ...newUser
+    } = await this.userRepository.save(createdUser);
+    return newUser;
+  }
+}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './services/user.service';
 import { UserController } from './controllers/user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './entities';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   providers: [UserService],
   controllers: [UserController],
 })
