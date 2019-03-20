15b085390cefa7b4ee340a898fc2982d8dde9fb9
Florian3k <24961583+Florian3k@users.noreply.github.com>
2019-03-20 17:21:48 +0100

    create /user/register handler

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { UserService } from '../services/user.service';
+import { RegisterUserResponseDto, RegisterUserRequestDto } from '../dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('register')
+  async register(
+    @Body() registerUserDto: RegisterUserRequestDto,
+  ): Promise<RegisterUserResponseDto> {
+    const { id, nickname, email } = await this.userService.createUser(
+      registerUserDto.nickname,
+      registerUserDto.password,
+      registerUserDto.email,
+    );
+
+    return { id, nickname, email };
+  }
+}
