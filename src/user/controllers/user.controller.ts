57eb34a38955567290de9f099334d1f9ec7c24d8
Florian3k <24961583+Florian3k@users.noreply.github.com>
2019-03-22 14:46:31 +0100

    create user login handler

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -1,10 +1,19 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, ForbiddenException } from '@nestjs/common';
 import { UserService } from '../services/user.service';
-import { RegisterUserResponseDto, RegisterUserRequestDto } from '../dto';
+import {
+  RegisterUserResponseDto,
+  RegisterUserRequestDto,
+  LoginUserRequestDto,
+  LoginUserResponseDto,
+} from '../dto';
+import { AuthService } from '../services/auth.service';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly authService: AuthService,
+  ) {}
 
   @Post('register')
   async register(
@@ -19,5 +28,20 @@ export class UserController {
     return {
       user: { id, nickname, email },
     };
+  }
+
+  @Post('/login')
+  async login(
+    @Body() loginUserDto: LoginUserRequestDto,
+  ): Promise<LoginUserResponseDto> {
+    const user = await this.userService.findByCredentials(
+      loginUserDto.nickname,
+      loginUserDto.password,
+    );
+    if (!user) {
+      throw new ForbiddenException('Invalid credentials');
+    }
+    const token = this.authService.tokenSign({ user });
+    return { user, token };
   }
 }
