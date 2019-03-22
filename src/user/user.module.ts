cd5faa4b0e593f0837d8ce37f58eb8bbc4690297
Florian3k <24961583+Florian3k@users.noreply.github.com>
2019-03-22 14:15:06 +0100

    create user service

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,11 @@ import { UserService } from './services/user.service';
 import { UserController } from './controllers/user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entities';
+import { AuthService } from './services/auth.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
-  providers: [UserService],
+  providers: [UserService, AuthService],
   controllers: [UserController],
 })
 export class UserModule {}
