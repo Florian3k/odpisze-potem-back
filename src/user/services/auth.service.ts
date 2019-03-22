8c97fe28fb021851699d486a9824e2db0a0c959a
Florian3k <24961583+Florian3k@users.noreply.github.com>
2019-03-22 14:22:03 +0100

    create tokenSign method

--- src/user/services/auth.service.ts
+++ src/user/services/auth.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { TokenPayloadModel } from '../models/token-payload.model';
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  tokenSign(tokenPayload: TokenPayloadModel): string {
+    return jwt.sign(tokenPayload, process.env.JWT_SECRET as string);
+  }
+}
