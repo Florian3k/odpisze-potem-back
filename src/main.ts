35a579668cf4e0e003a251d297f13f4bd1ba13b8
Florian3k <24961583+Florian3k@users.noreply.github.com>
2019-03-23 17:50:21 +0100

    create basic swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('odpisze-potem api')
+    .setVersion('0.0.1')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('docs', app, swaggerDocument);
+
   await app.listen(process.env.NODE_PORT as string);
 }
 bootstrap();
