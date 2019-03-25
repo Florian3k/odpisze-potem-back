aceb5533c627dd661e6d5d5960b42b05cb91f65e
Florian3k <24961583+Florian3k@users.noreply.github.com>
2019-03-25 21:33:11 +0100

    add swagger anotation to user model

--- src/user/models/user.model.ts
+++ src/user/models/user.model.ts
@@ -1,5 +1,12 @@
+import { ApiModelProperty } from '@nestjs/swagger';
+
 export class UserModel {
+  @ApiModelProperty()
   id: string;
+
+  @ApiModelProperty()
   nickname: string;
+
+  @ApiModelProperty()
   email: string;
 }
