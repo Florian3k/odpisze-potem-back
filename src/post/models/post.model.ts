94230dbe66a0699e134de45e48e9bc193fdbf402
Florian3k <24961583+Florian3k@users.noreply.github.com>
2019-03-25 21:44:07 +0100

    add swagger annotations to post model

--- src/post/models/post.model.ts
+++ src/post/models/post.model.ts
@@ -1,6 +1,15 @@
+import { ApiModelProperty } from '@nestjs/swagger';
+
 export class PostModel {
+  @ApiModelProperty()
   id: string;
+
+  @ApiModelProperty()
   title: string;
+
+  @ApiModelProperty()
   content: string;
+
+  @ApiModelProperty()
   createdAt: Date;
 }
