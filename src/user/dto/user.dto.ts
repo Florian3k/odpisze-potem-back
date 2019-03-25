a044b7a10dcf0252ac1030637b8515fddcb87a0d
Florian3k <24961583+Florian3k@users.noreply.github.com>
2019-03-25 19:56:54 +0100

    add validation to register user dto

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,9 +1,17 @@
 // tslint:disable: max-classes-per-file
 import { UserModel } from '../models';
+import { IsString, IsEmail, IsAlphanumeric, MinLength } from 'class-validator';
 
 export class RegisterUserRequestDto {
+  @IsAlphanumeric()
+  @MinLength(3)
   nickname: string;
+
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @MinLength(8)
   password: string;
 }
 
