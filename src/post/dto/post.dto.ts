7bd2df3da73bd3a56c9a4c66fb7f0434963fd81f
Florian3k <24961583+Florian3k@users.noreply.github.com>
2019-03-25 21:43:15 +0100

    add swagger annotations to post dtos

--- src/post/dto/post.dto.ts
+++ src/post/dto/post.dto.ts
@@ -1,10 +1,13 @@
 // tslint:disable: max-classes-per-file
+import { ApiModelProperty } from '@nestjs/swagger';
 import { PostModel } from '../models';
 
 export class GetAllPostsResponseDto {
+  @ApiModelProperty()
   posts: PostModel[];
 }
 
 export class GetPostResponseDto {
+  @ApiModelProperty()
   post?: PostModel;
 }
