879701dced164473c584bb250a984416b7c44eb3
Florian3k <24961583+Florian3k@users.noreply.github.com>
2019-03-25 20:32:48 +0100

    add swagger annotations to user dtos

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,33 +1,43 @@
 // tslint:disable: max-classes-per-file
 import { UserModel } from '../models';
 import { IsString, IsEmail, IsAlphanumeric, MinLength } from 'class-validator';
+import { ApiModelProperty } from '@nestjs/swagger';
 
 export class RegisterUserRequestDto {
   @IsAlphanumeric()
   @MinLength(3)
+  @ApiModelProperty()
   nickname: string;
 
   @IsEmail()
+  @ApiModelProperty()
   email: string;
 
   @IsString()
   @MinLength(8)
+  @ApiModelProperty()
   password: string;
 }
 
 export class RegisterUserResponseDto {
+  @ApiModelProperty()
   user: UserModel;
 }
 
 export class LoginUserRequestDto {
+  @ApiModelProperty()
   @IsString()
   nickname: string;
 
   @IsString()
+  @ApiModelProperty()
   password: string;
 }
 
 export class LoginUserResponseDto {
+  @ApiModelProperty()
   user: UserModel;
+
+  @ApiModelProperty()
   token: string;
 }
