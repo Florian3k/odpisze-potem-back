2f4493019d2ccfc2126c2354d6275389863c4896
Florian3k <24961583+Florian3k@users.noreply.github.com>
2019-03-27 13:04:38 +0100

    create UserMiddleware

--- src/user/models/token-payload.model.ts
+++ src/user/models/token-payload.model.ts
@@ -1,5 +1,11 @@
 import { UserModel } from './user.model';
 
+declare module 'express' {
+  export interface Request {
+    tokenPayload?: TokenPayloadModel;
+  }
+}
+
 export class TokenPayloadModel {
   user: UserModel;
 }
