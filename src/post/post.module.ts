ed85e653d228e4f0b608df4fe5151083c6c16437
Florian3k <24961583+Florian3k@users.noreply.github.com>
2019-03-27 17:56:52 +0100

    use UserMiddleware

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,12 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostController } from './controllers/post.controller';
 import { PostService } from './services/post.service';
 import { PostEntity } from './entities';
+import { UserMiddleware } from '../user/middlewares/user.middleware';
+import { AuthService } from '../user/services/auth.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([PostEntity])],
   controllers: [PostController],
-  providers: [PostService],
+  providers: [PostService, AuthService],
 })
-export class PostModule {}
+export class PostModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(UserMiddleware).forRoutes(PostController);
+  }
+}
